6e56338c8d88beb0dd35d275b1a07c1f6851c9ef
KFukushige65 <fukushigekeisuke@net77-dhcp238.sfc.keio.ac.jp>
2016-10-20 12:40:58 +0900

    練習問題5，6の追加

--- scripts:app.js
+++ scripts:app.js
@@ -3,13 +3,32 @@
      result = a / b
      return result;
     }
+     
      function toMILE(km){
 　　　let result;
 　　　result = div(km, 1.6)
 　　　return result;
 　　}    
-function toKM(mile){
-let result;
-result = mile * 1.6
-return result
+  
+  function toKM(mile){
+  let result;
+  result = mile * 1.6
+  return result
+   }
+
+function nand(a, b){
+  let c = a && b;
+  return !c; 
 }
+let a = nand(true, true); // false
+
+function nor(a, b){
+  let c = a || b;
+  return !c; 
+}
+
+function xor(a, b){
+  let c = a 
+}
+
+

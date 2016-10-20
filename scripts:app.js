0f8d2d09152cd574e008875f0504b18b90b375e6
KFukushige65 <fukushigekeisuke@net77-dhcp238.sfc.keio.ac.jp>
2016-10-20 11:49:07 +0900

    練習問題４の追加　少々変更あり

---
+++ scripts:app.js
@@ -0,0 +1,15 @@
+   function div(a, b){
+     let result;
+     result = a / b
+     return result;
+    }
+     function toMILE(km){
+　　　let result;
+　　　result = div(km, 1.6)
+　　　return result;
+　　}    
+function toKM(mile){
+let result;
+result = mile * 1.6
+return result
+}
